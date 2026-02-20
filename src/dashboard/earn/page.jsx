0c5c265497811e4a477a7f1b8413544d3667e888
Sky28702 <prateekrai462@outlook.com>
2026-02-20 07:33:27 +0530

    Sidebar Style fixed , dashboard seperate page added

---
+++ src/dashboard/earn/page.jsx
@@ -0,0 +1,7 @@
+export default function Earn() {
+  return (
+    <>
+      <section>Earn</section>
+    </>
+  );
+}
---
+++ src/dashboard/transaction/page.jsx
@@ -0,0 +1,7 @@
+export default function Transaction() {
+  return (
+    <>
+      <section>Transaction</section>
+    </>
+  );
+}
